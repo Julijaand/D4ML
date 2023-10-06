9e604bacc1a49647bde8c528321bf1b1bf04c4cc
Julijaand <113669109+Julijaand@users.noreply.github.com>
2023-10-06 10:01:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,20 +4,6 @@ provider "aws" {
 resource "aws_security_group" "JuliasSecurityGroup" {
     name        = "JuliasSecurityGroup"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["84.15.188.85/32"]
-  }
-
   egress {
     from_port   = 80
     to_port     = 80
